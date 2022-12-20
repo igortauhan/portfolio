208e7a1142bea5df56f0e7fb490ba958d61ff657
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-12-20 11:06:47 -0300

    Frontend: add font icons in social and footer component

--- frontend/src/app/component/social/social.component.ts
+++ frontend/src/app/component/social/social.component.ts
@@ -9,7 +9,13 @@ import { Social } from 'src/app/model/social.model';
 export class SocialComponent {
   @Input() public socials?: Social[];
 
+  private fontAwesomePrefix: string = 'fa fa-brands fa-';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  protected getClassName(socialName?: string): string {
+    return this.fontAwesomePrefix + socialName?.toLowerCase();
+  }
 }
